28a7a6d077b1c9b1ee61d510a8b724d7c5dfeab1
G1Hu6 <r1ruru219@gmail.com>
2025-01-20 20:58:04 +0530

    skip test

--- src/test/java/com/security/SpringbootSecurityApplicationTests.java
+++ src/test/java/com/security/SpringbootSecurityApplicationTests.java
@@ -1,29 +1,13 @@
 package com.security;
 
-import com.security.entities.UserEntity;
-import com.security.services.JWTService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootSecurityApplicationTests {
 
-	@Autowired
-	private JWTService jwtService;
-
 	@Test
 	void contextLoads() {
-		UserEntity userEntity = UserEntity.builder()
-				.id(2L)
-				.email("user@gmail.com")
-				.password("243wrw")
-				.build();
-		String token = jwtService.generateAccessToken(userEntity);
-		System.out.println(token);
-
-		Long id = jwtService.getUserIdFromToken(token);
-		System.out.println(id);
 	}
 
 }
